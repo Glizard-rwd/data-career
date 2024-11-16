cf7fbc46aa087db5381dc6f116d04e390868d38e
Glizard-rwd <s3915155@rmit.edu.vn>
2024-11-16 14:21:49 +0700

    update mysql file

--- function-procedure.sql
+++ function-procedure.sql
@@ -72,4 +72,92 @@ select o.orderDate, count(distinct o.orderNumber) as orderNumber, sum((od.quanti
 join orderdetails od on o.orderNumber = od.orderNumber
 group by o.orderDate
 order by totalPricePerDay desc;
+
+
+
+SELECT
+    YEAR(o.orderDate) as orderYear,
+    MONTH(o.orderDate) AS orderMonth,
+    COUNT(DISTINCT o.orderNumber) AS orderNumber,
+    SUM(od.quantityOrdered * od.priceEach) AS totalPricePerMonth
+FROM
+    orders o
+        JOIN
+    orderdetails od
+    ON
+        o.orderNumber = od.orderNumber
+GROUP BY
+    YEAR(o.orderDate),
+    MONTH(o.orderDate)
+ORDER BY
+    totalPricePerMonth DESC;
+
+
+
+# find orders has max and min money in each month
+select
+    year(o.orderDate) as orderYear,
+    month(o.orderDate) as orderMonth,
+    o.orderNumber,
+    sum(od.quantityOrdered * od.priceEach) as orderPrice
+from orders o join orderdetails od on o.orderNumber = od.orderNumber
+group by
+    year(o.orderDate),
+    month(o.orderDate),
+    o.orderNumber
+order by orderYear, orderMonth, orderPrice desc;
+
+select * from orders;
+
+# shipped ratio in each month
+with monthlyTotalOrders as
+    (select
+        year(o1.orderDate) as year,
+        month(o1.orderDate) as month,
+        count(distinct o1.orderNumber) as count
+    from orders o1
+    join orderdetails o2 on o1.orderNumber = o2.orderNumber
+    group by year(o1.orderDate), month(o1.orderDate))
+select
+    year(o.orderDate) as orderYear,
+    month(o.orderDate) as orderMonth,
+    count(o.orderNumber) as shippedOrders,
+    o1.count as totalOrder,
+    count(o.orderNumber) / o1.count as shippedRatio
+from orders o join monthlyTotalOrders o1
+    on year(o.orderDate) = o1.year and month(o.orderDate) = o1.month
+where o.status = 'Shipped'
+group by
+    year(o.orderDate), month(o.orderDate), o1.count
+order by orderYear, orderMonth;
+
+# calculate monthly profit
+select
+    year(orderDate),
+    month(orderDate),
+    sum(od.priceEach * od.quantityOrdered) as totalMonthlyProfit
+from orders o join orderdetails od on o.orderNumber = od.orderNumber
+group by month(orderDate), year(orderDate)
+order by totalMonthlyProfit desc;
+
+
 select * from orderdetails;
+
+# most favourite product
+select
+    p.productCode,
+    p.productName,
+    sum(distinct od.quantityOrdered) as totalQuantityOrdered
+from orderdetails od join products p on od.productCode = p.productCode
+group by p.productCode, productName;
+
+select
+    year(o.orderDate) as year,
+    month(o.orderDate) as month,
+    p.productCode,
+    p.productName,
+    sum(od.quantityOrdered) as orderTotal
+from orders o join orderdetails od on o.orderNumber = od.orderNumber
+join products p on od.productCode = p.productCode
+group by year, month, p.productCode, p.productName
+order by year, month, orderTotal desc;
